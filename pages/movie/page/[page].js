493ea9631640e3634927caa24cfb2e2862d223b8
Esteban Milicich <edmilicich96@gmail.com>
2021-05-07 15:10:47 -0300

    solución fixed al cambiar de página  en la home

---
+++ pages/movie/page/[page].js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Head from 'next/head'
+
+import AppLayout from 'Components/AppLayout'
+import Searcher from 'Components/Searcher'
+
+import Card from 'Components/Card'
+
+export const GenrePage = ({ data }) => {
+  return (
+    <AppLayout>
+      <Head>
+        <title>Movies</title>
+        <link rel="icon" href="https://img.icons8.com/ultraviolet/40/000000/movie-projector.png" />
+      </Head>
+      <Searcher />
+      <Card data={data} />
+    </AppLayout>
+
+  )
+}
+
+export default GenrePage
+
+export async function getServerSideProps (context) {
+  const { query } = context
+  const apiResponse = await fetch(`https://yts.mx/api/v2/list_movies.json?sort_by=year&page=${query.page}`)
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return { props: props }
+  }
+}
